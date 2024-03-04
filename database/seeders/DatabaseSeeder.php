03285ee2ea87e117ca15162a07244826940a671a
GabrielDSousa <gabrielramos.email@gmail.com>
2024-03-04 11:09:28 -0300

    feat: Adicionar Readme, documentação da API e seed de usuário
    Este commit adiciona um Readme contendo instruções para configurar o sistema localmente, documentação da API e um arquivo challenge.md com o enunciado do desafio. Além disso, inclui um usuário para desenvolvimento local na seed da base de dados.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Dev user for local development
+        // password: password
         if (!app()->environment('production')) {
             $user = \App\Models\User::factory()->create([
-                'name' => 'Developer User',
-                'email' => 'dev@abc.com',
+                'email' => 'email@email.com',
             ]);
         }
 
-        \App\Models\Product::factory(10)->create();
+        $users = \App\Models\User::factory(10)->create();
+        $products = \App\Models\Product::factory(10)->create();
+        $sales = \App\Models\Sale::factory(10)->recycle($users)->create();
+        foreach ($sales as $key => $sale) {
+            $sale->products()->attach($products->random()->id, ['quantity' => rand(1, 10)]);
+        }
     }
 }
